0972a857be9c42bdb993e5c7e408bde80466d523
Joffre Veloz <joffreandres11@gmail.com>
2024-05-11 01:24:42 -0500

    Update get-project-id.ts

--- src/actions/company/get-project-id.ts
+++ src/actions/company/get-project-id.ts
@@ -3,29 +3,37 @@
 import { auth } from "@/auth.config";
 
 export const getProjectId = async (id: string) => {
-  const session = await auth();
-  if (!session?.user) {
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      throw new Error("Debe estar autenticado");
+    }
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/projects/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session.user.token}`,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(`Error al obtener el proyecto: ${res.statusText}`);
+    }
+
+    const project = await res.json();
+
+    return {
+      status: project.status,
+      project: project.data,
+    };
+  } catch (error: any) {
     return {
       status: false,
-      message: "Debe de estar autenticado",
+      message: error.message || "Error al obtener el proyecto",
     };
   }
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/projects/${id}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${session?.user?.token}`,
-      },
-      next: { revalidate: 10000 },
-    }
-  );
-  const project = await res.json();
-
-  return {
-    status: true,
-    project: project,
-  };
 };
